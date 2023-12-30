512aa1f6900a165ec7bc4995c89e56c4f76bde9f
asthaagarwal01 <astha2agarwal5@gmail.com>
2023-12-30 15:57:04 +0530

    fetchlist

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+async function fetchlist() {
+    try {
+        const response = await fetch('https://s3.amazonaws.com/open-to-cors/assignment.json');
+        const data = await response.json();
+        return Object.values(data.products).sort((a, b) => b.popularity - a.popularity);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
 fetchlist().then(display);
